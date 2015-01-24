6c2b3e7440f8c169530a5e387e97835afc05698c
tuxik <tuxik.ru@gmail.com>
2015-01-24 13:34:29 +0000

    Add yoho - Yeoman app

---
+++ 2/yoho/app/scripts/controllers/about.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yohoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yohoApp
+ */
+angular.module('yohoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+    'Nirvana'  ,
+        'Satori'
+    ];
+  });
